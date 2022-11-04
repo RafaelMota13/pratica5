0c9513f59349aaff3cc887f2e780e03533fff0c9
Rafael <faelmota7@gmail.com>
2022-11-04 20:34:29 -0300

    pratica

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿//bool deveExecutar = true;
+
+//while (deveExecutar) // enquanto
+//{
+    //Console.WriteLine("Etec Adolpho Berezin");    
+//}
+
+  // loop infinito
+
+int numeroAtual = 1; //iterações
+int limite = 5;
+int valorDaSoma = 0;
+
+while (numeroAtual <= limite) // condição de continuidade
+{
+    //0
+
+
+    valorDaSoma = valorDaSoma + numeroAtual;
+    numeroAtual = numeroAtual + 2;
+
+    //1
+}
+
+Console.ForegroundColor= ConsoleColor.DarkGreen;
+
+Console.WriteLine(valorDaSoma);
+Console.WriteLine(numeroAtual);
+
+Console.ResetColor();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
